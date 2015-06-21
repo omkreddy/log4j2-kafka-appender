2e8504600c3fce4d2ca85addcaccedf4fdaf7b44
Manikumar Reddy O <manikumar.reddy@gmail.com>
2015-06-21 22:06:03 +0530

    Support given for layouts

--- src/main/java/com/github/omkreddy/log4j2/appender/KafkaAppender.java
+++ src/main/java/com/github/omkreddy/log4j2/appender/KafkaAppender.java
@@ -38,6 +38,7 @@ import org.apache.logging.log4j.core.config.plugins.Plugin;
 import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
 import org.apache.logging.log4j.core.config.plugins.PluginElement;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.layout.SerializedLayout;
 import org.apache.logging.log4j.core.util.Booleans;
 
 @SuppressWarnings("unused")
@@ -48,17 +49,22 @@ public final class KafkaAppender extends AbstractAppender {
 	private String topic;
     private boolean syncsend;
     
-	protected KafkaAppender(String name, Filter filter, boolean ignoreExceptions, KafkaProducer<String, String> producer, String topic, boolean syncsend) {
-		super(name, filter, null, ignoreExceptions);
+	protected KafkaAppender(String name, Filter filter, Layout<? extends Serializable> layout, boolean ignoreExceptions, KafkaProducer<String, String> producer, String topic, boolean syncsend) {
+		super(name, filter, layout, ignoreExceptions);
 		this.producer = producer;
 		this.topic = topic;
 		this.syncsend = syncsend;
 	}
 
 	@PluginFactory
-	public static KafkaAppender createAppender(@PluginAttribute("name") final String name, @PluginElement("Filter") final Filter filter,
-			@PluginAttribute("ignoreExceptions") final String ignore, @PluginAttribute("topic") final String topic,  @PluginAttribute("enable") String enable,
-			@PluginAttribute("syncsend") String syncsend, @PluginElement("Properties") final Property[] properties) {
+	public static KafkaAppender createAppender(@PluginAttribute("name") final String name,
+		                                       @PluginElement("Filter") final Filter filter,
+			                                   @PluginAttribute("ignoreExceptions") final String ignore,
+			                                   @PluginAttribute("topic") final String topic,
+			                                   @PluginAttribute("enable") String enable,
+			                                   @PluginAttribute("syncsend") String syncsend,
+			                                   @PluginElement("Layout") Layout<? extends Serializable> layout,
+			                                   @PluginElement("Properties") final Property[] properties) {
 		boolean ignoreExceptions = Booleans.parseBoolean(ignore, true);
 		boolean enableKafka = Booleans.parseBoolean(enable, true);
 		boolean sync = Booleans.parseBoolean(syncsend, false);
@@ -66,7 +72,6 @@ public final class KafkaAppender extends AbstractAppender {
 		KafkaProducer<String, String> producer = null;
 		Map<String, Object> props = new HashMap<String, Object>();
 		for (Property property : properties) {
-			System.out.println(property.getValue());
 			props.put(property.getName(), property.getValue());
 		}
 		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
@@ -75,7 +80,11 @@ public final class KafkaAppender extends AbstractAppender {
 		if(enableKafka)
 		   producer = new KafkaProducer<String, String>(props);
 	
-		return new KafkaAppender(name, filter, ignoreExceptions, producer , topic, sync);
+		if(layout == null) {
+			layout = SerializedLayout.createLayout();
+		}
+
+		return new KafkaAppender(name, filter, layout, ignoreExceptions, producer , topic, sync);
  
 	}
 
@@ -90,21 +99,14 @@ public final class KafkaAppender extends AbstractAppender {
 	public void append(LogEvent event) {
 		try {
 			if (producer != null) {
-			    Future<RecordMetadata> result = producer.send(new ProducerRecord<String, String>(topic, event.getMessage().getFormattedMessage()));
+			    Future<RecordMetadata> result = producer.send(new ProducerRecord<String, String>(topic, getLayout().toSerializable(event).toString()));
 			    if(syncsend)
 			    	result.get();
 			}
 		} catch (final Exception e) {
-			System.out.println("Error");
 			LOGGER.error("Unable to write to kafka for appender [{}].",  this.getName(), e);
 			throw new AppenderLoggingException("Unable to write to kafka in appender: " + e.getMessage(), e);
 		} finally {
 		}
 	}
-
-	@Override
-	public Layout<? extends Serializable> getLayout() {
-		return null;
-	}
-
 }
